d60c71a2b7f9940cab57f101a9be72c72f0fe7f8
costel450 <91177617+costel450@users.noreply.github.com>
2021-11-01 10:44:22 +0200

    Update locatie.js

--- locatie.js
+++ locatie.js
@@ -1,21 +1,19 @@
 get_geo_position();
-function_on_succes(posision)
-{
-    document.getElementById("latitude").innerHTML="Latitude="+posision.coords.latitude;
-    document.getElementById("longitude").innerHTML="Longitude="+posision.coords.longitude;
-    document.getElementById("acc").innerHTML="Acuracy="+posision.coords.acuracy;
-    document.getElementById("altitude").innerHTML="Altitude="+posision.coords.altitude;
-    var latlon=posision.coords.latitude+ "," +posision.coords.longitude;
-    var img_url="https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=YOUR_KEY";
-    document.getElementById("map").innerHTML="<img src='"+img_url+"'>";
+function on_success(position){
+document.getElementById("latitude").innerHTML = "Latitude = " + position.coords.latitude;
+document.getElementById("longitude").innerHTML = "Longitude= " + position.coords.longitude;
+document.getElementById("acc").innerHTML = "Accuracy = "+position.coords.accuracy;
+document.getElementById("altitude").innerHTML = "Altitude = "+position.coords.altitude;
+var latlon = position.coords.latitude + "," + position.coords.longitude;
+var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=YOUR_KEY";
+document.getElementById("map").innerHTML = "<img src='"+img_url+"'>";
 }
-function_on_error(e)
+function on_error(e)
 {
-    document.getElementById("text").innerHTML = e.message;
+document.getElementById("text").innerHTML = e.message;
 }
-
-var geo_params={
-    enableHighAccuracy:true,
+var geo_params = {
+enableHighAccuracy:true,
 timeout:1000,
 maximumAge:0
 };
